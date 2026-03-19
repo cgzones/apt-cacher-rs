3cf8d255a67157a4777235a59016f1ea509da71b
Christian Göttsche <cgzones@googlemail.com>
2026-03-19 18:49:54 +0100

    Replace OnceLock<PhantomData<bool>> with AtomicBool
    Reduces stack size from 4 bytes to 1 byte.

--- src/log_once.rs
+++ src/log_once.rs
@@ -1,35 +1,39 @@
 #[macro_export]
 macro_rules! warn_once {
     ($($t:tt)*) => {{
-        static FIRED: std::sync::OnceLock<std::marker::PhantomData<bool>> =
-            std::sync::OnceLock::new();
-        FIRED.get_or_init(|| {
-            log::warn!($($t)*);
-            std::marker::PhantomData
-        });
+        static FIRED: std::sync::atomic::AtomicBool = std::sync::atomic::AtomicBool::new(false);
+
+        match FIRED.compare_exchange(false, true, std::sync::atomic::Ordering::Relaxed, std::sync::atomic::Ordering::Relaxed) {
+            Ok(false) => log::warn!($($t)*),
+            Ok(true) => unreachable!("value must never change from false to true"),
+            Err(_) => {}
+        }
     }};
 }
 
 #[macro_export]
 macro_rules! warn_once_or_info {
     ($($t:tt)*) => {{
-        static FIRED: std::sync::OnceLock<std::marker::PhantomData<bool>> =
-            std::sync::OnceLock::new();
-        log::log!(match FIRED.set(std::marker::PhantomData) {
-            Ok(()) => log::Level::Warn,
-            Err(std::marker::PhantomData) => log::Level::Info,
-        },$($t)*);
+        static FIRED: std::sync::atomic::AtomicBool = std::sync::atomic::AtomicBool::new(false);
+
+        let level = match FIRED.compare_exchange(false, true, std::sync::atomic::Ordering::Relaxed, std::sync::atomic::Ordering::Relaxed) {
+            Ok(false) => log::Level::Warn,
+            Ok(true) => unreachable!("value must never change from false to true"),
+            Err(_) => log::Level::Info,
+        };
+        log::log!(level, $($t)*);
     }};
 }
 
 #[macro_export]
 macro_rules! info_once {
     ($($t:tt)*) => {{
-        static FIRED: std::sync::OnceLock<std::marker::PhantomData<bool>> =
-            std::sync::OnceLock::new();
-        FIRED.get_or_init(|| {
-            log::info!($($t)*);
-            std::marker::PhantomData
-        });
+        static FIRED: std::sync::atomic::AtomicBool = std::sync::atomic::AtomicBool::new(false);
+
+        match FIRED.compare_exchange(false, true, std::sync::atomic::Ordering::Relaxed, std::sync::atomic::Ordering::Relaxed) {
+            Ok(false) => log::info!($($t)*),
+            Ok(true) => unreachable!("value must never change from false to true"),
+            Err(_) => {}
+        }
     }};
 }
