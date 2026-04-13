7eb8fa7c6edd497e62bb117d9d3fe68c46efc3f3
Christian Göttsche <cgzones@googlemail.com>
2026-04-13 13:45:18 +0200

    Add warn_once_or_debug!()

--- src/log_once.rs
+++ src/log_once.rs
@@ -26,6 +26,20 @@ macro_rules! warn_once_or_info {
 }
 
 #[macro_export]
+macro_rules! warn_once_or_debug {
+    ($($t:tt)*) => {{
+        static FIRED: std::sync::atomic::AtomicBool = std::sync::atomic::AtomicBool::new(false);
+
+        let level = match FIRED.compare_exchange(false, true, std::sync::atomic::Ordering::Relaxed, std::sync::atomic::Ordering::Relaxed) {
+            Ok(false) => log::Level::Warn,
+            Ok(true) => unreachable!("value must never change from false to true"),
+            Err(_) => log::Level::Debug,
+        };
+        log::log!(level, $($t)*);
+    }};
+}
+
+#[macro_export]
 macro_rules! info_once {
     ($($t:tt)*) => {{
         static FIRED: std::sync::atomic::AtomicBool = std::sync::atomic::AtomicBool::new(false);
